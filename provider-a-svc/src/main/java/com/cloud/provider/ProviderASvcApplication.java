9c91d8957f9b741f1bf686f0b54becc5584a1fc3
flybesttop <1602803469@qq.com>
2022-07-15 00:58:31 +0800

    commit

--- provider-a-svc/src/main/java/com/cloud/provider/ProviderASvcApplication.java
+++ provider-a-svc/src/main/java/com/cloud/provider/ProviderASvcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProviderSvcApplication {
+public class ProviderASvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProviderSvcApplication.class, args);
+        SpringApplication.run(ProviderASvcApplication.class, args);
     }
 
 }
